b7bace27fba87010a4c8ec4217d44e1ca0132bc5
Masterv98 <masterv1998@gmail.com>
2019-12-05 15:08:26 -0300

    Update Nano.ino

--- Nano.ino
+++ Nano.ino
@@ -18,12 +18,34 @@ SoftwareSerial s(5,6);
 DHT dht(DHTPIN, DHTTYPE);
 //------------------------------------//
 
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_BMP280.h>
 
+//-------------BMP280----------------//
+#define BMP_SCK  (13)
+#define BMP_MISO (12)
+#define BMP_MOSI (11)
+#define BMP_CS   (10)
+
+//Adafruit_BMP280 bmp; // I2C
+//Adafruit_BMP280 bmp(BMP_CS); // hardware SPI
+Adafruit_BMP280 bmp(BMP_CS, BMP_MOSI, BMP_MISO,  BMP_SCK);
+//------------------------------------//
  
 void setup() {
+
   s.begin(115200);    /* Comunicacao Serial entre plataformas*/
   pinMode(A0,INPUT);  /* Pino para o DHT*/
   dht.begin();        /* Inicio de comunicacao com o DHT*/
+
+
+/* Default settings from datasheet. */
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
   
 }
 
@@ -45,7 +67,9 @@ void loop() {
  
   // Calcula Sensasao termica
   float hic = dht.computeHeatIndex(t, u, false);
-  //bmp.readAltitude(1013.25)); 
+
+  //Le o sensor de chuva resistivo
+  float pa = bmp.readPressure();
 
   //Le o sensor de chuva resistivo
   float pp = analogRead(A0);
@@ -59,7 +83,7 @@ if (isnan(h) || isnan(t) || isnan(pa) || isnan(pp)) {       /* Caso algo estaja 
   root["temp"] = t; //Temperadura
   root["u"] = u;   //Umidade
   root["hi"] = hic; //Sensacao termica
-
+  root["pa"]= pa; //pressao
   root["pp"]= pp; //precipitacao
   
  
